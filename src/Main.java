5346951c28d00196949268bb35be79e800d8d20f
KM Botha <st10258494@vcconnect.edu.za>
2023-08-22 09:04:16 +0200

    Anything

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,48 @@
 import javax.swing.*;
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
 
         boolean run = true;
         do {
-            int input = Integer.parseInt(JOptionPane.showInputDialog("STUDENT MANAGEMENT APPLICATION" +
+            Scanner menu = new Scanner(System.in);
+            System.out.println("STUDENT MANAGEMENT APPLICATION" +
                     "\n***************************" +
-                    "\nEnter (1) ro launch menu or any other key to exit"));
+                    "\nEnter (1) ro launch menu or any other key to exit");
+            int input = Integer.parseInt(menu.nextLine());
             if (input == 1) {
 
-                int menu = Integer.parseInt(JOptionPane.showInputDialog("Please select one of the following menu items:"
-                    + "\n(1) Capture a new student."
-                    + "\n(2) Search for a student."
-                    + "\n(3) Delete a student."
-                    + "\n(4) Print student report."
-                    + "\n(5) Exit Application."));
+                System.out.println("Please select one of the following menu items:"
+                        + "\n(1) Capture a new student."
+                        + "\n(2) Search for a student."
+                        + "\n(3) Delete a student."
+                        + "\n(4) Print student report."
+                        + "\n(5) Exit Application.");
+                int menuOption = Integer.parseInt(menu.nextLine());
+
+                if (menuOption == 1){
+                    Student.saveStudent();
+                } else if (menuOption == 2) {
+                    Student.searchStudent();
+                } else if (menuOption == 3) {
+                    Student.deleteStudent();
+                } else if (menuOption == 4) {
+                    Student.studentReport();
+                } else if(menuOption == 5) {
+                    run = false;
+                }
+                else {
+                    System.out.print("Please enter a valid option");
+                    menuOption = Integer.parseInt(menu.nextLine());
+                }
 
             }
             else{
                 run = false;
             }
+
 
         } while (run);
     }
--- src/Student.java
+++ src/Student.java
@@ -1,9 +1,65 @@
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Student {
 
-    public static void SaveStudent(){
+    public static ArrayList<StudentData> students = new ArrayList<>();
+    public static boolean saveStudent(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("CAPTURE A NEW STUDENT"
+        + "\n*********************************");
+
+        System.out.print("Enter the student ID: ");
+        String id = input.nextLine();
+
+        System.out.print("Enter the student name: ");
+        String name = input.nextLine();
+
+        System.out.print("Enter the student age: ");
+        int age = Integer.parseInt(input.nextLine());
+        if (age < 16){
+            System.out.print("You have entered a incorrect age!!!"
+            + "\nPlease re-enter the student age >>");
+            age = Integer.parseInt(input.nextLine());
+        }
+
+        System.out.print("Enter the student email: ");
+        String email = input.nextLine();
+
+        System.out.print("Enter the student course");
+        String course = input.nextLine();
+
+        StudentData student = new StudentData(id, name, email, course, age);
+        students.add(student);
+
+        System.out.print("Details successfully captured.");
+        System.out.print("Enter (1) to launch menu or any other key to exit.");
+        String exit = input.nextLine();
+
+        return exit.equals("1");
+    }
+
+    public static void searchStudent(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a student id to search: ");
+        System.out.print("-----------------------------------------");
+        idSearch
 
     }
 
-    public static void
+    public static void deleteStudent(){
+
+    }
+
+    public static void studentReport(){
+
+    }
+
+    public static void exitStudentApplication (){
+
+    }
 
 }
