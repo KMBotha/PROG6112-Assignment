c025d17dc79fd9909890aa065408573c6fb293fd
Kelly <st10258494@vcconnect.edu.za>
2023-08-24 11:20:25 +0200

    Finalized Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Student {
@@ -40,7 +41,7 @@ public class Student {
         return exit.equals("1");
     }
 
-    public static void searchStudent(){
+    public static boolean searchStudent(){
         Scanner input = new Scanner(System.in);
         StudentData student = new StudentData();
         String message = "";
@@ -49,7 +50,7 @@ public class Student {
         String idSearch = input.nextLine();
         System.out.print("\n-----------------------------------------");
 
-        boolean contains = false;
+        boolean contains;
         for (int i = 0; i < students.size(); i++){
             if (students.get(i).getId().contains(idSearch)){
                 contains = true;
@@ -67,32 +68,68 @@ public class Student {
             }
             else{
                 message = "\nStudent with Student Id: " + input + " was not found!";
-                System.out.print("Enter a student id to search: ");
+                System.out.print("\nEnter a student id to search: ");
                 idSearch = input.nextLine();
                 System.out.print("\n-----------------------------------------");
             }
         }
         System.out.print(message);
         System.out.print("\nEnter (1) to launch menu or any other key to exit.");
-        int exit = Integer.parseInt(input.nextLine());
+        String exit = input.nextLine();
 
-        if (ext == 1){
-            Main.
-        }
-
-
+        return exit.equals("1");
     }
 
-    public static void deleteStudent(){
+    public static boolean deleteStudent(){
+        Scanner input =  new Scanner(System.in);
+        System.out.print("Enter the id to delete: ");
+        String id = input.nextLine();
+        StudentData student = new StudentData();
+
+        boolean contains = false;
+        for (int i = 0; i < students.size(); i++) {
+            if (students.get(i).getId().contains(id)){
+                contains = true;
+                student = students.get(i);
+            } else{
+                contains = false;
+            }
+
+            if(contains){
+                System.out.print("Are you sure you want to delete student " + id + " from the system? Yes (y) to delete.");
+                String delete = input.nextLine().toLowerCase();
+                if (delete.equals("y")){
+                    students.remove(i);
+                }
+            }
+        }
+        System.out.print("\nEnter (1) to launch menu or any other key to exit.");
+        String exit = input.nextLine();
+
+        return exit.equals("1");
 
     }
 
     public static void studentReport(){
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 0; i < students.size(); i++) {
+            System.out.print("\nSTUDENT " + i);
+            System.out.print("\n------------------------------------");
+            System.out.print("\nSTUDENT ID: " + students.get(i).getId()
+                    + "\nSTUDENT NAME: " + students.get(i).getFirstName()
+                    + "\nSTUDENT AGE: " + students.get(i).getAge()
+                    + "\nSTUDENT EMAIL: " + students.get(i).getEmail()
+                    + "\nSTUDENT COURSE: " + students.get(i).getCourse());
+
+        }
 
     }
 
-    public static void exitStudentApplication (){
+    public static boolean exitStudentApplication (){
 
+        String exit = "1";
+        return exit.equals("1");
     }
 
 }
